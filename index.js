a964519d4e2d9080f8c4d4d1ca21d6a7a81d3578
Romain DELIMAL <rom.delimal@gmail.com>
2023-01-02 19:30:14 +0100

    withCopyPaste

--- index.js
+++ index.js
@@ -331,43 +331,43 @@ let classmates = [
 
 // import puppeteer from "/node_modules/puppeteer-core/lib/cjs/puppeteer/node/Browser.js";
 
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
-process.setMaxListeners(Infinity);
+// process.setMaxListeners(Infinity);
 
-const args = [
-  '--disable-gpu',
-  '--no-sandbox',
-]
+// const args = [
+//   '--disable-gpu',
+//   '--no-sandbox',
+// ]
 
-for (let i = 0; i < classmates.length; i++) {
-  (async () => {
-    // console.log(classmates[i].name);
+// for (let i = 0; i < classmates.length; i++) {
+//   (async () => {
+//     // console.log(classmates[i].name);
 
-    const browser = await puppeteer.launch({
-      headless     : true,
-      handleSIGINT : false,
-      args         : args,
-    })
+//     const browser = await puppeteer.launch({
+//       headless     : true,
+//       handleSIGINT : false,
+//       args         : args,
+//     })
 
-    const page = await browser.newPage();
-    // await page.setDefaultNavigationTimeout(0);
-    await page.goto(`${classmates[i].github_url}`, { waitUntil: 'networkidle2'});
+//     const page = await browser.newPage();
+//     // await page.setDefaultNavigationTimeout(0);
+//     await page.goto(`${classmates[i].github_url}`, { waitUntil: 'networkidle2'});
 
-    await page.waitForSelector('.js-yearly-contributions h2', {timeout: 0});
-    let data = await page.evaluate(function() {
-      let contributions = document.querySelector('.js-yearly-contributions h2').innerText;
-      // console.log(contributions);
-      return contributions;
-    });
-    await page.close();
+//     await page.waitForSelector('.js-yearly-contributions h2', {timeout: 0});
+//     let data = await page.evaluate(function() {
+//       let contributions = document.querySelector('.js-yearly-contributions h2').innerText;
+//       // console.log(contributions);
+//       return contributions;
+//     });
+//     await page.close();
 
-    classmates[i].contributions = await data.replace(/\D+/g, '');
-    console.log(classmates[i]);
-    // console.log(classmates[i].contributions);
-    await browser.close();
-  })();
-};
+//     classmates[i].contributions = await data.replace(/\D+/g, '');
+//     console.log(classmates[i]);
+//     // console.log(classmates[i].contributions);
+//     await browser.close();
+//   })();
+// };
 
 // start();
 
@@ -414,41 +414,55 @@ for (let i = 0; i < classmates.length; i++) {
 // function insertContributions(classmate) {
 //   classmate.contributions = githubScrape(classmate);
 // }
+// const fs = require('fs');
 
-// function insertHtml(classmate) {
-//   this.document.getElementById('card-container').insertAdjacentHTML('beforeend',
-//   `<div class="swiper-slide card">
-//       <div class="card-content">
-//         <div class="image">
-//           <img src="${classmate.photo_url}" alt="">
-//         </div>
+// const myArr = JSON.parse(data);
 
-//         <div class="media-icons">
-//           <i class="fa-brands fa-linkedin"></i>
-//           <i class="fa-brands fa-github"></i>
-//         </div>
+// import { myArr } from './data.json';
+// import data from './data.json' assert { type: 'JSON' };
+// console.log(data);
+const myArr = [
+{"name":"Mario Aouane","photo_url":"https://avatars.githubusercontent.com/u/115270856?v=4","github_url":"https://github.com/Mario-dev-42","linkedin_url":"","contributions":"69"},{"name":"Robin Rettien","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665072255/zfsfmzn9icpxkxusegly.jpg","github_url":"https://github.com/RobinRTN","linkedin_url":"https://www.linkedin.com/in/robin-rettien-b30067115/","contributions":"218"},{"name":"Charlotte Oswald","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665344616/yznhsqumvxan0rupnhg4.jpg","github_url":"https://github.com/CharlotteO9","linkedin_url":"https://www.linkedin.com/in/charlotte-oswald-09681aa5","contributions":"296"},{"name":"Shayneze Menegady","photo_url":"https://avatars.githubusercontent.com/u/62299446?v=4","github_url":"https://github.com/Shayneze","linkedin_url":"https://www.linkedin.com/in/shayneze-menegady/","contributions":"170"},{"name":"Nikolaz Stacey","photo_url":"https://avatars.githubusercontent.com/u/74938003?v=4","github_url":"https://github.com/nikolaz90","linkedin_url":"https://www.linkedin.com/in/nikolaz-s/","contributions":"519"},{"name":"Hugo Pouchoux","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665343498/vekrllkan1tqzvklgjiz.jpg","github_url":"https://github.com/Pouchouxx","linkedin_url":"https://www.linkedin.com/in/hugo-pouchoux","contributions":"139"},{"name":"Anne Ly","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665132136/fjc4ibutfjtc5qztzap7.jpg","github_url":"https://github.com/littleannie","linkedin_url":"https://www.linkedin.com/in/annely75/","contributions":"406"},{"name":"Romain Delimal","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1671031692/dw3o8ajav5rtu8jpgwsx.jpg","github_url":"https://github.com/romprd","linkedin_url":"https://www.linkedin.com/in/romain-delimal/","contributions":"391"},{"name":"Ahmadou Dia","photo_url":"https://avatars.githubusercontent.com/u/96949079?v=4","github_url":"https://github.com/ahmadia23","linkedin_url":"https://www.linkedin.com/in/ahmadou-dia-47337710a/","contributions":"1260"},{"name":"Julien Ferreira","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1670845116/hrjarzvsgmszm2uubiui.jpg","github_url":"https://github.com/Julien-Ferreira","linkedin_url":"https://www.linkedin.com/in/julien-ferreira-89b49925a/","contributions":"112"},{"name":"William Swinnen","photo_url":"https://avatars.githubusercontent.com/u/115130952?v=4","github_url":"https://github.com/williamswinnen","linkedin_url":"https://www.linkedin.com/in/williamswinnen/","contributions":"96"},{"name":"Aaron Delabays","photo_url":"https://avatars.githubusercontent.com/u/115422001?v=4","github_url":"https://github.com/AaronDelabays","linkedin_url":"https://www.linkedin.com/in/aaron-d-59757b31/","contributions":"102"},{"name":"Gabriel de Monts","photo_url":"https://avatars.githubusercontent.com/u/113995804?v=4","github_url":"https://github.com/gabmonts","linkedin_url":"https://www.linkedin.com/in/gabriel-de-monts/","contributions":"174"},{"name":"Gaspard Fauchille","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665332162/j13rib7t6gt4xbiulgqv.jpg","github_url":"https://github.com/Gauchille","linkedin_url":"https://www.linkedin.com/in/gaspard-fauchille-29992517b","contributions":"228"},{"name":"Mehdi Rachid","photo_url":"https://avatars.githubusercontent.com/u/115085959?v=4","github_url":"https://github.com/MehdiR02","linkedin_url":"https://www.linkedin.com/in/mehdi-rachid-3a42a8171","contributions":"125"},{"name":"Julien Nguyen","photo_url":"https://avatars.githubusercontent.com/u/104355406?v=4","github_url":"https://github.com/Naruki95","linkedin_url":"https://www.linkedin.com/in/julien-nguyen-8aa749145/","contributions":"137"},{"name":"Fabiola Hora Ordinola","photo_url":"https://avatars.githubusercontent.com/u/48128895?v=4","github_url":"https://github.com/fhora","linkedin_url":"https://www.linkedin.com/in/fabiola-hora-ordinola","contributions":"154"},{"name":"Raphael Assaf","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665472288/xp4lla0l8s2j4ysvlj9n.jpg","github_url":"https://github.com/Raphassaf01","linkedin_url":"https://www.linkedin.com/in/raphaël-assaf-912b501a3/","contributions":"184"},{"name":"Pierre Pétel","photo_url":"https://avatars.githubusercontent.com/u/112730785?v=4","github_url":"https://github.com/pierreptl","linkedin_url":"https://www.linkedin.com/in/pierre-petel/","contributions":"127"},{"name":"Marine Wetzel","photo_url":"https://avatars.githubusercontent.com/u/115414856?v=4","github_url":"https://github.com/marinewtzl","linkedin_url":"https://www.linkedin.com/in/marinewetzel/","contributions":"133"},{"name":"Kevin Blakimé","photo_url":"https://avatars.githubusercontent.com/u/115102708?v=4","github_url":"https://github.com/blkevin","linkedin_url":"https://www.linkedin.com/in/kevin-blakim%C3%A9-b3522a80/","contributions":"190"},{"name":"Alban de La Rochefordière","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665117611/xlgahjdus4ba2ulnokkx.jpg","github_url":"https://github.com/dlralban","linkedin_url":"https://www.linkedin.com/in/dlralban/","contributions":"250"},{"name":"Pauline Gouttiere","photo_url":"https://avatars.githubusercontent.com/u/106176972?v=4","github_url":"https://github.com/GHPops","linkedin_url":"https://www.linkedin.com/in/pauline-goutti%C3%A8re-82736162/","contributions":"218"},{"name":"Hugo Chipotel","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1667466121/qx0xtpaltwynlekqt2he.jpg","github_url":"https://github.com/Hugochipotel","linkedin_url":"","contributions":"75"},{"name":"Sébastien Cuthbert","photo_url":"https://avatars.githubusercontent.com/u/33867598?v=4","github_url":"https://github.com/Blueton23","linkedin_url":"https://www.linkedin.com/in/s%C3%A9bastien-cuthbert-1a7432220/","contributions":"177"},{"name":"Benjamin Doberset","photo_url":"https://avatars.githubusercontent.com/u/36986044?v=4","github_url":"https://github.com/Benjamin-Doberset","linkedin_url":"https://www.linkedin.com/in/benjamin-doberset/","contributions":"190"},{"name":"Gauthier Marchand","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665326756/hvpyn5fv9pmbfadtfbme.jpg","github_url":"https://github.com/garchand","linkedin_url":"https://www.linkedin.com/in/gauthier-marchand/","contributions":"201"},{"name":"Gautier David","photo_url":"https://avatars.githubusercontent.com/u/103845189?v=4","github_url":"https://github.com/GautierDavid","linkedin_url":"https://www.linkedin.com/in/gautierdavid93","contributions":"577"},{"name":"Eliott Norie","photo_url":"https://avatars.githubusercontent.com/u/115416510?v=4","github_url":"https://github.com/MyrmecoMania","linkedin_url":"https://www.linkedin.com/in/eliott-norie-7b307123a/","contributions":"358"},{"name":"Thibaut Roland","photo_url":"https://avatars.githubusercontent.com/u/115095838?v=4","github_url":"https://github.com/ThibautRoland","linkedin_url":"https://www.linkedin.com/in/thibaut-roland2/","contributions":"199"},{"name":"Emeline Barthe","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1670512535/mwqgyaeffknprexu6jxm.jpg","github_url":"https://github.com/embarthe51","linkedin_url":"https://www.linkedin.com/in/emelinebarthe/","contributions":"258"},{"name":"Arthur Foucaud","photo_url":"https://avatars.githubusercontent.com/u/115413024?v=4","github_url":"https://github.com/ArthurFoucaud","linkedin_url":"https://www.linkedin.com/in/arthur-foucaud/","contributions":"284"},{"name":"William Passera","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665137908/kvgo7hienojexbrywyjr.jpg","github_url":"https://github.com/Will-p1275","linkedin_url":"https://www.linkedin.com/in/william-passera-722ba4245/","contributions":"216"},{"name":"Élodie Bourguignon","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1670420493/a37pwmxs3vqqnela2s4u.jpg","github_url":"https://github.com/ElodieBou97","linkedin_url":"https://www.linkedin.com/in/%C3%A9lodie-bourguignon-a9b947236/","contributions":"151"},{"name":"Franck Lemarchand","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1670235617/tddm97ebcaysis1s2m5s.jpg","github_url":"https://github.com/FranckL93","linkedin_url":"https://www.linkedin.com/in/francklemarchand/","contributions":"331"},{"name":"Joel Irmiahu","photo_url":"https://avatars.githubusercontent.com/u/115413668?v=4","github_url":"https://github.com/painkex","linkedin_url":"https://www.linkedin.com/in/joel-irmiahu-a3b69025a","contributions":"225"},{"name":"Anne Rubio","photo_url":"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1665328758/wrue7f0kw1muvsalsgrj.jpg","github_url":"https://github.com/AnneRubio","linkedin_url":"https://www.linkedin.com/in/anne-rubio-93100-ar93/","contributions":"187"},{"name":"Maxence Robinet","photo_url":"https://avatars.githubusercontent.com/u/107369283?v=4","github_url":"https://github.com/saint-james-fr","linkedin_url":"https://www.linkedin.com/in/maxence-robinet-b71376135","contributions":"459"}
+];
 
-//         <div class="name-profession">
-//           <span class="name">${classmate.name}</span>
-//           <span class="profession">Web Developer</span>
-//         </div>
+let sortedRank = myArr.sort((r1, r2) => (parseInt(r1.contributions) < parseInt(r2.contributions)) ? 1 : (parseInt(r1.contributions) > parseInt(r2.contributions)) ? -1 : 0);
 
-//         <div class="stats">
-//           <div class="stat-category contributions" >
-//             <span id="${classmate.name.replace(/ /g,'-')}" class="number green">${classmate.contributions}</span>
-//             <span class="text">Contributions</span>
-//           </div>
+console.log(sortedRank)
 
-//           <div class="stat-category rank">
-//             <span class="number">#1</span>
-//             <span class="text">rank</span>
-//           </div>
-//         </div>
+sortedRank.forEach((classmate, i) => {
+  document.getElementById('card-container').insertAdjacentHTML('beforeend',
+  `<div class="swiper-slide card">
+      <div class="card-content">
+        <div class="image">
+          <img src="${classmate.photo_url}" alt="">
+        </div>
 
-//         <div class="button">
-//           <button class="aboutMe">About Me</button>
-//         </div>
-//       </div>
-//     </div>`
-//   );
-// }
+        <div class="media-icons">
+          <i class="fa-brands fa-linkedin"></i>
+          <i class="fa-brands fa-github"></i>
+        </div>
+
+        <div class="name-profession">
+          <span class="name">${classmate.name}</span>
+          <span class="profession">Web Developer</span>
+        </div>
+
+        <div class="stats">
+          <div class="stat-category contributions" >
+            <span id="${classmate.name.replace(/ /g,'-')}" class="number green">${classmate.contributions}</span>
+            <span class="text">Contributions</span>
+          </div>
+
+          <div class="stat-category rank">
+            <span class="number">#${i+1}</span>
+            <span class="text">rank</span>
+          </div>
+        </div>
+
+        <div class="button">
+          <button class="aboutMe">About Me</button>
+        </div>
+      </div>
+    </div>`
+  );
+});
